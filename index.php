458570a86e53ac8a30020b3a287f3f8a254a617e
mij3as <62897477+mij3as@users.noreply.github.com>
2020-12-12 16:21:51 +0300

    dshhydujdjdujdujdsu
    udjjmdcdmkmikflogflp;lokjhtgv

--- index.php
+++ index.php
@@ -1,97 +1,12 @@
-<!-- //$ head -->
-<?php include("includes/head.html");?>
+<?php 
+include("includes/head.html");
+include("includes/nav.html");
+?>
 <title>aperon | Home</title>
-<!-- //! head -->
 
-<!-- //$ =-=-=-=-=-=-=-=-=-== navigation  ==-=-=-=-=-=-=-=-=-= -->
-<nav class="navbar navbar-expand-lg ">
-	<a class="navbar-brand" href="index.php">
-		<svg width="50" height="38" viewBox="0 0 50 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path fill-rule="evenodd" clip-rule="evenodd" d="M50 13.245C50 20.5601 44.07 26.4901 36.755 26.4901C34.2388 26.4901 32.0616 25.947 30.1325 24.812V37.7483H23.5099V17.1732C23.0745 16.3986 22.6465 15.5903 22.2184 14.7546C20.5193 11.4373 19.1419 9.42335 17.7726 8.21951C16.5588 7.15247 15.2406 6.62252 13.245 6.62252C9.58752 6.62252 6.62252 9.58752 6.62252 13.245C6.62252 16.9025 9.58751 19.8676 13.245 19.8676V26.4901C5.93001 26.4901 0 20.5601 0 13.245C0 5.93 5.93 0 13.245 0C16.7357 0 19.6394 1.0429 22.1451 3.24572C23.2641 4.22943 24.2674 5.41421 25.2033 6.76022C27.4732 2.72545 31.7958 0 36.755 0C44.07 0 50 5.93001 50 13.245ZM30.1325 15.3635C30.9569 16.6827 31.7023 17.634 32.4487 18.3207C33.5666 19.3492 34.7749 19.8676 36.755 19.8676C40.4125 19.8676 43.3775 16.9025 43.3775 13.245C43.3775 9.58751 40.4125 6.62252 36.755 6.62252C33.0975 6.62252 30.1325 9.58751 30.1325 13.245V15.3635Z" class="svg-logo" fill="url(#lgrad)" />
-			<path d="M30.1311 24.8117V15.3632C30.6527 16.1977 31.1426 16.885 31.6212 17.4512V25.5508C31.1081 25.3407 30.6119 25.0946 30.1311 24.8117Z" fill="url(#paint0_linear)" />
-			<path d="M22.2162 14.7555C20.5171 11.4383 19.1397 9.42433 17.7704 8.22049L22.1429 3.2467C23.2696 4.2372 24.279 5.43154 25.2205 6.78906C24.1421 8.7058 23.5271 10.918 23.5271 13.2739L23.5271 17.202C23.0505 16.3636 22.6833 15.6674 22.2162 14.7555Z" fill="url(#paint1_radial)" />
-			<defs>
-				<linearGradient id="paint0_linear" x1="28.8066" y1="20.5288" x2="31.7867" y2="20.5287" gradientUnits="userSpaceOnUse">
-					<stop stop-opacity="0.76" />
-					<stop offset="1" stop-opacity="0" />
-				</linearGradient>
-				<radialGradient id="paint1_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(26.3223 11.756) rotate(-168.232) scale(4.05882 12.1726)">
-					<stop offset="0.3125" stop-opacity="0.76" />
-					<stop offset="1" stop-opacity="0" />
-				</radialGradient>
-				<linearGradient id="lgrad" x1="0%" y1="100%" x2="100%" y2="0%">
-					<stop offset="0%" style="stop-color:rgb(110,46,199);stop-opacity:1" />
-					<stop offset="50%" style="stop-color:rgb(216,0,134);stop-opacity:1" />
-					<stop offset="75%" style="stop-color:rgb(237,0,97);stop-opacity:1" />
-					<stop offset="100%" style="stop-color:rgb(242,34,61);stop-opacity:1" />
-				</linearGradient>
-			</defs>
-		</svg>
-	</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon">
-			<i class="fa fa-bars"></i>
-		</span>
-	</button>
 
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item active">
-				<a class="nav-link" href="#">home </a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">about</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">how to use</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					avatar creators
-				</a>
-				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="#">Among Us</a>
-					<a class="dropdown-item" href="#">Fortnite</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="#">Comming soon</a>
-				</div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">partners</a>
-			</li>
-		</ul>
-		<div class="dropdown ap-right ap-check">
-			<a class="nav-link dropdown-toggle no-after" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				<i class="fas fa-cog " style="font-size: 20px;"></i>
-			</a>
-			<div class="dropdown-menu dropdown-menu-right">
-				<form class="px-4 py-3" method="get">
-					<p>Scrol handel</p>
-					<!-- c -->
-				</form>
-				<div class="dropdown-divider"></div>
-				<p style="font-size: 10px;" class="align-middle">© aperon</p>
-			</div>
-		</div>
-		<div class="dropdown ap-right ">
-			<a class="nav-link dropdown-toggle no-after" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				<i class="fas fa-sign-in-alt" style="font-size: 20px;"></i>
-			</a>
-			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-				<a class="dropdown-item" href="#">Sign in</a>
-				<a class="dropdown-item" href="#">Sign out</a>
-				<div class="dropdown-divider"></div>
-				<p style="font-size: 10px;" class="align-middle">© aperon</p>
-			</div>
-		</div>
 
-	</div>
-</nav>
-<!-- //! =-=-=-=-=-=-=-=-=-== navigation  ==-=-=-=-=-=-=-=-=-= -->
-<!-- //$ =-=-=-=-=-=-=-=-=-== main section ==-=-=-=-=-=-=-=-=-= -->
+<!-- //$ main -->
 <main id="main">
 	<div class="container-fluid">
 		<div class="row">
@@ -109,8 +24,8 @@
 		</div>
 	</div>
 </main>
-<!-- //! =-=-=-=-=-=-=-=-=-== main section ==-=-=-=-=-=-=-=-=-= -->
-<!-- //$ =-=-=-=-=-=-=-=-=-== parnters section ==-=-=-=-=-=-=-=-=-= -->
+<!-- //!  mian -->
+<!-- //$ partners -->
 <div id="parnters" class="block">
 	<h1>partners</h1>
 	<svg width="85.5" height="9" viewBox="0 0 57 6" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -119,19 +34,19 @@
 	</svg>
 	<div class="swiper-container">
 		<div class="swiper-wrapper">
-			<div class="swiper-slide"><a href="https://account.mojang.com/"><img src="img/1.png" alt="Mojang"></a></div>
-			<div class="swiper-slide"><a href="https://google.com/"><img src="img/2.png" alt="Google"></a></div>
-			<div class="swiper-slide"><a href="https://minecraft.net/"><img src="img/3.png" alt="Minecraft"></a></div>
-			<div class="swiper-slide"><a href="http://www.innersloth.com/"><img src="img/8.png" alt="Among Us"></a></div>
-			<div class="swiper-slide"><a href="http://www.innersloth.com/"><img src="img/5.png" alt="INNER SLOTH"></a></div>
-			<div class="swiper-slide"><a href="https://github.com/"><img src="img/6.png" alt="Github"></a></div>
-			<div class="swiper-slide"><a href="https://namemc.com/"><img src="img/4.png" alt="Name Mc"></a></div>
-			<div class="swiper-slide"><a href="https://figma.com/"><img src="img/7.png" alt="Figma"></a></div>
+			<div class="swiper-slide"><a target="_blank" href="https://account.mojang.com/"><img src="img/1.png" alt="Mojang" ></a></div>
+			<div class="swiper-slide"><a target="_blank" href="https://google.com/"><img src="img/2.png" alt="Google"></a></div>
+			<div class="swiper-slide"><a target="_blank" href="https://minecraft.net/"><img src="img/3.png" alt="Minecraft"></a></div>
+			<div class="swiper-slide"><a target="_blank" href="http://www.innersloth.com/"><img src="img/8.png" alt="Among Us"></a></div>
+			<div class="swiper-slide"><a target="_blank" href="http://www.innersloth.com/"><img src="img/5.png" alt="INNER SLOTH"></a></div>
+			<div class="swiper-slide"><a target="_blank" href="https://github.com/"><img src="img/6.png" alt="Github"></a></div>
+			<div class="swiper-slide"><a target="_blank" href="https://namemc.com/"><img src="img/4.png" alt="Name Mc"></a></div>
+			<div class="swiper-slide"><a target="_blank" href="https://figma.com/"><img src="img/7.png" alt="Figma"></a></div>
 		</div>
 	</div>
 </div>
-<!-- //! =-=-=-=-=-=-=-=-=-== parnters section ==-=-=-=-=-=-=-=-=-= -->
-<!-- //$ =-=-=-=-=-=-=-=-=-== about ==-=-=-=-=-=-=-=-=-= -->
+<!-- //! partners -->
+<!-- //$ about -->
 <div id="about" class="block">
 	<div class="container-fluid">
 		<div class="row">
@@ -150,12 +65,9 @@
 		</div>
 	</div>
 </div>
-<!-- //! =-=-=-=-=-=-=-=-=-== about ==-=-=-=-=-=-=-=-=-= -->
-<!-- //$ =-=-=-=-=-=-=-=-=-== contact ==-=-=-=-=-=-=-=-=-= -->
+<!-- //! about -->
+<!-- //$ contact -->
 <div id="contact" class="block">
-	<!-- h1
-		  h6
-		hr	 -->
 	<h1 class="text-uppercase font-weight-bold">Contact Us</h1>
 	<svg width="85.5" class="he2 linear" height="9" viewBox="0 0 57 6" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path d="M4.05284 0H30.2629L26.7364 3.43117H0.526367L4.05284 0Z" />
@@ -191,110 +103,10 @@
 		</form>
 	</div>
 </div>
-<!-- //! =-=-=-=-=-=-=-=-=-== contact ==-=-=-=-=-=-=-=-=-= -->
-<!-- //$ =-=-=-=-=-=-=-=-=-== footer ==-=-=-=-=-=-=-=-=-= -->
-<footer class="page-footer font-small unique-color-dark ap-footer">
-	<div class="container text-center text-md-left mt-5">
-		<div class="row mt-3">
-			<div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-				<svg class="footer-logo" width="252.99" height="191" viewBox="0 0 50 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M50 13.245C50 20.5601 44.07 26.4901 36.755 26.4901C34.2388 26.4901 32.0616 25.947 30.1325 24.812V37.7483H23.5099V17.1732C23.0745 16.3986 22.6465 15.5903 22.2184 14.7546C20.5193 11.4373 19.1419 9.42335 17.7726 8.21951C16.5588 7.15247 15.2406 6.62252 13.245 6.62252C9.58752 6.62252 6.62252 9.58752 6.62252 13.245C6.62252 16.9025 9.58751 19.8676 13.245 19.8676V26.4901C5.93001 26.4901 0 20.5601 0 13.245C0 5.93 5.93 0 13.245 0C16.7357 0 19.6394 1.0429 22.1451 3.24572C23.2641 4.22943 24.2674 5.41421 25.2033 6.76022C27.4732 2.72545 31.7958 0 36.755 0C44.07 0 50 5.93001 50 13.245ZM30.1325 15.3635C30.9569 16.6827 31.7023 17.634 32.4487 18.3207C33.5666 19.3492 34.7749 19.8676 36.755 19.8676C40.4125 19.8676 43.3775 16.9025 43.3775 13.245C43.3775 9.58751 40.4125 6.62252 36.755 6.62252C33.0975 6.62252 30.1325 9.58751 30.1325 13.245V15.3635Z" class="svg-logo" fill="url(#lgrad)" />
-					<path d="M30.1311 24.8117V15.3632C30.6527 16.1977 31.1426 16.885 31.6212 17.4512V25.5508C31.1081 25.3407 30.6119 25.0946 30.1311 24.8117Z" fill="url(#paint0_linear)" />
-					<path d="M22.2162 14.7555C20.5171 11.4383 19.1397 9.42433 17.7704 8.22049L22.1429 3.2467C23.2696 4.2372 24.279 5.43154 25.2205 6.78906C24.1421 8.7058 23.5271 10.918 23.5271 13.2739L23.5271 17.202C23.0505 16.3636 22.6833 15.6674 22.2162 14.7555Z" fill="url(#paint1_radial)" />
-					<defs>
-						<linearGradient id="paint0_linear" x1="28.8066" y1="20.5288" x2="31.7867" y2="20.5287" gradientUnits="userSpaceOnUse">
-							<stop stop-opacity="0.76" />
-							<stop offset="1" stop-opacity="0" />
-						</linearGradient>
-						<radialGradient id="paint1_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(26.3223 11.756) rotate(-168.232) scale(4.05882 12.1726)">
-							<stop offset="0.3125" stop-opacity="0.76" />
-							<stop offset="1" stop-opacity="0" />
-						</radialGradient>
-						<linearGradient id="lgrad" x1="0%" y1="100%" x2="100%" y2="0%">
-							<stop offset="0%" style="stop-color:rgb(110,46,199);stop-opacity:1" />
-							<stop offset="50%" style="stop-color:rgb(216,0,134);stop-opacity:1" />
-							<stop offset="75%" style="stop-color:rgb(237,0,97);stop-opacity:1" />
-							<stop offset="100%" style="stop-color:rgb(242,34,61);stop-opacity:1" />
-						</linearGradient>
-					</defs>
-				</svg>
-			</div>
-			<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-				<h6 class="text-uppercase font-weight-bold">links</h6>
-				<svg width="85.5" class="he2 linear" height="9" viewBox="0 0 57 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M4.05284 0H30.2629L26.7364 3.43117H0.526367L4.05284 0Z" />
-					<path d="M30.2629 2.09682H56.4732L52.9467 5.52799H26.7364L30.2629 2.09682Z" />
-					<defs>
-						<linearGradient id="lgrad" x1="0%" y1="100%" x2="100%" y2="0%">
-							<stop offset="0%" style="stop-color:rgb(110,46,199);stop-opacity:1" />
-							<stop offset="100%" style="stop-color:rgb(242,34,61);stop-opacity:1" />
-						</linearGradient>
-					</defs>
-				</svg>
-				<p style="padding-top: 10px;">
-					<a href="amongus/">Among us A.C. </a>
-				</p>
-				<p>
-					<a href="#!">fortnite A.C.</a>
-				</p>
-				<p>
-					<a href="#!">about</a>
-				</p>
-				<p>
-					<a href="#!">contant</a>
-				</p>
-			</div>
-			<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-				<h6 class="text-uppercase font-weight-bold">Useful links</h6>
-				<svg width="85.5" class="he2 linear" height="9" viewBox="0 0 57 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M4.05284 0H30.2629L26.7364 3.43117H0.526367L4.05284 0Z" />
-					<path d="M30.2629 2.09682H56.4732L52.9467 5.52799H26.7364L30.2629 2.09682Z" />
-					<defs>
-						<linearGradient id="lgrad" x1="0%" y1="100%" x2="100%" y2="0%">
-							<stop offset="0%" style="stop-color:rgb(110,46,199);stop-opacity:1" />
-							<stop offset="100%" style="stop-color:rgb(242,34,61);stop-opacity:1" />
-						</linearGradient>
-					</defs>
-				</svg>
-				<br>
-				<p style="padding-top: 10px;">
-					<a href="#!">pravicy and policy</a>
-				</p>
-				<p>
-					<a href="#!">terms of service</a>
-				</p>
-				<p>
-					<a href="#!">contant</a>
-				</p>
-			</div>
-			<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-				<h6 class="text-uppercase font-weight-bold">Contact</h6>
-				<svg width="85.5" class="he2 linear" height="9" viewBox="0 0 57 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M4.05284 0H30.2629L26.7364 3.43117H0.526367L4.05284 0Z" />
-					<path d="M30.2629 2.09682H56.4732L52.9467 5.52799H26.7364L30.2629 2.09682Z" />
-					<defs>
-						<linearGradient id="lgrad" x1="0%" y1="100%" x2="100%" y2="0%">
-							<stop offset="0%" style="stop-color:rgb(110,46,199);stop-opacity:1" />
-							<stop offset="100%" style="stop-color:rgb(242,34,61);stop-opacity:1" />
-						</linearGradient>
-					</defs>
-				</svg>
-				<p style="padding-top: 10px;">
-					<i class="fas fa-home mr-3"></i> Turkey, Gaziantep</p>
-				<p><i class="fas fa-envelope mr-3"></i> mij3us@gmail.com</p>
-				<p><i class="fab fa-instagram mr-3"></i> <a href="https://www.instagram.com/mij3us">@mij3us</a></p>
-			</div>
-		</div>
-	</div>
-	<div class="footer-copyright text-center py-3">
-		All Copyrights Reserved 2020 © |
-		<a href="https://aperon.co/"> aperon.co</a>
-	</div>
-</footer>
-<!-- //! =-=-=-=-=-=-=-=-=-== footer ==-=-=-=-=-=-=-=-=-= -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-<script src="plugins/swiper.js"></script>
+<!-- //! contact -->
+<!-- //$ footer -->
+<!-- //! footer -->
+<?php include("img/footer.html");?>
 <script>
 	function goToGames() {
 		location.replace("games.php");
@@ -340,6 +152,9 @@
 		},
 	});
 </script>
+
+
+
 <!-- //$ =-=-=-=-=-=-=-=-=-==  ==-=-=-=-=-=-=-=-=-= -->
 <!-- //* ==>  <== -->
 <?php
